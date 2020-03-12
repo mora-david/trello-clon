f002b343f3b2c77a0deac34ee6da6a5f0cddbac2
david.mora <david.89.mora@gmail.com>
2020-03-11 19:52:43 -0600

    pruebas112332121111

--- core/views.py
+++ core/views.py
@@ -24,7 +24,7 @@ class UserViewSet(viewsets.ModelViewSet):
             permission_classes = (AllowAny,)
         else:
             permission_classes = (IsAdminUser,)
-        return permission_classes
+        return permission_classes,
 
 
     queryset = User.objects.all()
